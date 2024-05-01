bfc6600b0503b7e7d34d5ba3f9f7987e8c0af74b
VanThen60hz <nguyenvthang2409@gmail.com>
2024-05-01 14:12:01 +0700

    feature: add user api

--- src/main/java/com/dtu/socialnetwork/controller/UserController.java
+++ src/main/java/com/dtu/socialnetwork/controller/UserController.java
@@ -1,22 +1,69 @@
 package com.dtu.socialnetwork.controller;
 
 import com.dtu.socialnetwork.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
+    static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Thang", "Nguyen Van", "helloword@gmail.com", "password"));
+        users.add(new User(2, "Then", "Van", "helloworld@gmail.com", "password"));
+    }
+
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        List<User> users = new ArrayList<>();
-
-        users.add(new User("Thang", "Nguyen Van", "helloword@gmail.com", "password"));
-        users.add(new User("Then", "Van", "helloworld@gmail.com", "password"));
 
         return users;
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getUserById(@PathVariable("userId") Integer id) {
+        return users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping("/users")
+    public User CreateUser(@RequestBody User user) {
+        users.add(user);
+        return user;
+    }
+
+    @PatchMapping("/users/{userId}")
+    public User updateUser(@PathVariable("userId") Integer id, @RequestBody User user) {
+        User userUpdate = users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+        if (userUpdate != null) {
+            userUpdate.setFirstName(user.getFirstName());
+            userUpdate.setLastName(user.getLastName());
+            userUpdate.setEmail(user.getEmail());
+            userUpdate.setPassword(user.getPassword());
+        }
+
+
+        return userUpdate;
+    }
+
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable("userId") Integer id) {
+        User userDelete = users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+        if (userDelete != null) {
+            users.remove(userDelete);
+            return "Delete success!";
+        }
+        return "Can't find user to delete!";
     }
 }
--- src/main/java/com/dtu/socialnetwork/models/User.java
+++ src/main/java/com/dtu/socialnetwork/models/User.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
 
+    @JsonProperty("id")
+    private Integer id;
     @JsonProperty("first_name")
     private String firstName;
 
@@ -19,11 +21,51 @@ public class User {
     public User() {
     }
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(Integer id, String firstName, String lastName, String email, String password) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
